26e9a6d4b2a8e6e4d00e0ea9cd6a70e7db1cf748
Roner1-bit <58177688+Roner1-bit@users.noreply.github.com>
2020-12-28 16:49:38 +0200

    Database commit
    Multithreading will be added later upon creating more features of the app.

--- submission_functions.py
+++ submission_functions.py
@@ -1,3 +1,8 @@
+from Database import Database
+
+database = Database()
+
+
 def back(instance):
     instance.parent.parent.parent.manager.current = 'registered'
 
@@ -6,11 +11,20 @@ def register(instance):
     screen = instance.parent.parent.parent  # type: RegisterScreen
     if screen.verify_inputs():
         form_dict = screen.get_form_values()
-        # TODO: DB stuff goes here.
-
+        email_verification = database.create_an_account(form_dict["Email"], form_dict[
+            "Password"])  # This is to verify that the entered email isn't an existing email in the forms.
+        if (email_verification == "Email already exists"):
+            return "Email already exists"
+        picture_path = form_dict.pop("Picture")
+        name_on_database = str(database.primaryKeyCreator(form_dict["Email"].encode('utf-8'))) + picture_path[
+                                                                                                 -4:]  # This makes the file name of the media on the database with the PK specified with the user and after "+" is to get the picture extention.
+        database.upload_media(picture_path, name_on_database)
+        output=database.insertData(form_dict,"CustomersData")
+        return output
 
 def login(instance):
     screen = instance.parent.parent.parent  # type: LoginScreen
     if screen.verify_inputs():
         login_dict = screen.get_form_values()
         # TODO: DB stuff goes here.
+        database.login(login_dict["Email"],login_dict["Password"])
